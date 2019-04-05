d9a903da5761f9af90063d6dcace4840a8dbcb00
isakper <isakpersson971@hotmail.se>
2019-04-05 16:01:42 +0200

    fixed issues

--- Models/model_layers.py
+++ Models/model_layers.py
@@ -1,5 +1,11 @@
 from keras.models import load_model
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, metavar='Path',
+                    help='path of model')
+Path = parser.parse_args()
 
 def get_model_layers(path):
     model = load_model(path)
@@ -8,3 +14,11 @@ def get_model_layers(path):
             print(layer.name, layer.get_weights()[0].shape)
         except:
             print(layer.name)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, metavar='Path',
+                    help='path of model')
+    Path = parser.parse_args().model_path
+    get_model_layers(Path)

a632c9a0e6e7a845ed243e0992b85e5b83c11302
isakper <isakpersson971@hotmail.se>
2019-04-14 19:29:37 +0200

    validation data

--- Models/Instagram/VGG7.py
+++ Models/Instagram/VGG7.py
@@ -9,60 +9,47 @@ from keras.layers.core import Dropout
 from keras.layers.core import Dense
 from keras import backend as K
 
-class VGG7:
+class VGG7_model:
 	@staticmethod
 	def build(width, height, depth, classes):
 		# initialize the model along with the input shape to be
 		# inputShape with theano backend
 		model = Sequential()
-		inputShape = (depth, height, width)
-		chanDim = 1
+		inputShape = (depth, width, height)
 
 		# First block
-		model.add(Convolution2D(64, 3, 3, border_mode="same",
-						 input_shape=inputShape))
+		model.add(Convolution2D(64, 3, 3, border_mode="same", input_shape=inputShape, name= 'conv1-1'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
-		model.add(Convolution2D(64, 3, 3, border_mode="same"))
+		model.add(Convolution2D(64, 3, 3, border_mode="same", name= 'conv1-2'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
-		#model.add(Dropout(0.25))
 
 		# Second block
-		model.add(Convolution2D(128, 3, 3, border_mode="same",
-						 input_shape=inputShape))
+		model.add(Convolution2D(128, 3, 3, border_mode="same", name= 'conv2-1'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
-		model.add(Convolution2D(128, 3, 3, border_mode="same"))
+		model.add(Convolution2D(128, 3, 3, border_mode="same", name= 'conv2-2'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
-		#model.add(Dropout(0.25))
 
 		# Third block
-		model.add(Convolution2D(256, 3, 3,border_mode="same",
-						 input_shape=inputShape))
+		model.add(Convolution2D(256, 3, 3, border_mode="same", name= 'conv3-1'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
-		model.add(Convolution2D(256, 3, 3, border_mode="same"))
+		model.add(Convolution2D(256, 3, 3, border_mode="same", name= 'conv3-2'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
-		model.add(Convolution2D(256, 3, 3, border_mode="same"))
+		model.add(Convolution2D(256, 3, 3, border_mode="same", name= 'conv3-3'))
 		model.add(Activation("relu"))
-		#model.add(BatchNormalization(axis=chanDim))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
-		#model.add(Dropout(0.25))
 
 		# FC
 		model.add(Flatten())
-		model.add(Dense(2048))
+		model.add(Dense(1024))
 		model.add(Activation("relu"))
 
 
 		# softmax classifier
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
+
 
 		# return the constructed network architecture
 		return model

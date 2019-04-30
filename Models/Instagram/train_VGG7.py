6e3da85fd09ec981c59cd00ba9e1df3b90145321
Labb1 <fangming.yan.yolanda@gmail.com>
2019-04-30 08:29:43 +0200

    sporty vs feminine new learning rate

--- Models/Instagram/train_VGG7.py
+++ Models/Instagram/train_VGG7.py
@@ -13,10 +13,10 @@ img_width, img_height = 224, 224
 # initialize the number of epochs to train for, base learning rate,
 # and batch size
 NUM_EPOCHS = 25
-INIT_LR = 1e-2
+INIT_LR = 1e-3
 BS = 32
-num_images = 4000
-num_val_images = 400
+num_images = 5000
+num_val_images = 500
 
 model = fullVGGNet.build(width=img_width, height=img_height, depth=3, classes=2)
 opt = SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR / NUM_EPOCHS)
@@ -25,12 +25,12 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 
-train_datagen = image.ImageDataGenerator(rescale=1./255)
+train_datagen = image.ImageDataGenerator(horizontal_flip=True,rescale=1./255)
 
-train_gen = train_datagen.flow_from_directory('Instagram_dataset_noNoise/Train',
+train_gen = train_datagen.flow_from_directory('Feminine_VS_Sporty/Train',
                                               target_size = (img_width, img_height),
                                               batch_size = BS)
-val_gen = train_datagen.flow_from_directory('Instagram_dataset_noNoise/Validation',
+val_gen = train_datagen.flow_from_directory('Feminine_VS_Sporty/Validation',
                                             target_size = (img_width, img_height),
                                             batch_size = BS)
 
@@ -41,4 +41,4 @@ model.fit_generator(train_gen,
                     nb_val_samples = num_val_images,
                     nb_epoch=NUM_EPOCHS)
 
-model.save('Instagram_VGG7_NoNoise.h5')
+model.save('Instagram_fullVGG_Sporty.h5')
